9285fdc2d87fab462d92fc052ad14f70a6e980df
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-04-02 22:44:25 +0600

    function defined

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -21,8 +21,7 @@ class UserControllerTest extends TestCase
     }
 
     public function testUserControllerIndex()
-    {
-        
+    {    
         $response = $this->get('/users');
         $response->assertStatus(200);
         $response->assertViewIs('users.index');
